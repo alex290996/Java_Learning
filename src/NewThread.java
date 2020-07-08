cb4513ca79ea79ba4f7086bcee25ad4c99bd82e5
alex290996 <alex290996@mail.ru>
2020-07-08 15:00:09 +0300

    lisen 1 java

---
+++ src/NewThread.java
@@ -0,0 +1,41 @@
+public class NewThread implements Runnable{
+    String name;
+    Thread t;
+    boolean suspendFlag;
+
+        NewThread(String threadname){
+            name = threadname;
+            t = new Thread(this, name);
+            System.out.println("Новый поток" + t);
+            suspendFlag = false;
+            t.start();
+        }
+
+    @Override
+    public void run() {
+try {
+    for(int i = 15; i > 0; i--) {
+     System.out.println(name + ":" + i);
+     Thread.sleep(200);
+     synchronized (this) {
+         while(suspendFlag) {
+             wait();
+         }
+     }
+    }
+}
+catch (InterruptedException e) {
+    System.out.println(name + " прерван");
+}
+    }
+
+synchronized void mysuspend () {
+            suspendFlag = true;
+}
+
+synchronized void myresume () {
+            suspendFlag = false;
+            notify();
+}
+
+}
---
+++ src/SuspendResume.java
@@ -0,0 +1,34 @@
+public class SuspendResume {
+    public static void main (String args[]){
+        NewThread ob1 = new NewThread("Один");
+        NewThread ob2 = new NewThread("Два");
+
+    try{
+        Thread.sleep(1000);
+        ob1.mysuspend();
+        System.out.println("Приостановка потока один");
+        Thread.sleep(1000);
+        ob1.myresume();
+        System.out.println("Возобновление потока один");
+        ob2.mysuspend();
+        System.out.println("Приостановление потока один");
+        Thread.sleep(1000);
+        ob2.myresume();
+        System.out.println("Возабновление потока два");
+
+    } catch (InterruptedException e) {
+        e.printStackTrace();
+        System.out.println("Главный поток прерван");
+    }
+
+    try {
+        System.out.println("Ожидание завершения потоков");
+        ob1.t.join();
+        ob2.t.join();
+    } catch (InterruptedException e) {
+        e.printStackTrace();
+        System.out.println("Главный поток прерван");
+    }
+    System.out.println("Главный поток завершен");
+    }
+}
