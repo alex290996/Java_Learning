dddbf2eca1ff1c023221d8b5bd68627a18b41098
dev <alex290996@mail.ru>
2021-02-28 20:49:55 +0500

    BufferedReader

---
+++ src/BufferedReader.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class BufferedReader {
+   // BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    //String name = reader.readLine();
+   // String sAge = reader.readLine();
+   // int nAge = Integer.parseInt(sAge);
+
+    Scanner scanner = new Scanner(System.in);
+  //  String name = scanner.nextLine();
+  //  int age = scanner.nextInt();
+}
---
+++ src/Solution.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new java.io.InputStreamReader(System.in));
+        String a = reader.readLine();
+        String b = reader.readLine();
+        System.out.println(a + " проспонсировал " + b + ", и она стала известной певицей.");
+    }
+}
