69c8987bcc40d07ca771cc797f56016cf2fa17e5
dev <alex290996@mail.ru>
2021-03-13 16:55:29 +0500

    BigDecimals

--- src/TinyEdit.java
+++ src/TinyEdit.java
@@ -1,5 +1,8 @@
 //Простейший текстовый редактор
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 class TinyEdit {
     public static void main(String args[]) throws IOException {
 // создать поток ввода типа BufferedReader,
